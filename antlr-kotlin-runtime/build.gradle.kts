42d83f1c7ae3cfa47015ef13dcb0fd505534ccb9
Gabriel Piacenti <Gabriel.Piacenti@LibertyMutual.com>
2021-05-19 12:45:33 -0700

    updating to kotlin 1.5

--- antlr-kotlin-runtime/build.gradle.kts
+++ antlr-kotlin-runtime/build.gradle.kts
@@ -22,7 +22,7 @@ kotlin {
             dependencies {
                 implementation(kotlin("stdlib-common"))
                 implementation(kotlin("reflect"))
-                implementation("com.soywiz.korlibs.korio:korio:1.12.0")
+                implementation("com.soywiz.korlibs.korio:korio:2.0.10")
                 implementation("com.benasher44:uuid:0.1.0")
             }
         }
